c67e92b52cf2c0203c07b2a6d095ceccdaffb8cf
Wes Ostlund <wostlund25@my.whitworth.edu>
2022-11-13 22:09:12 -0800

    first commit

---
+++ source/main.cpp
@@ -0,0 +1,71 @@
+// Wes Ostlund
+// cs-273-2 mp4
+//
+#include<iostream>
+#include<fstream>
+#include <map>
+#include <vector>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+int main()
+{
+
+    std::map<string,int> numOccurences; // create a map with the key being a word and the stored value being the number of times that word occured
+    std::map<int, std::vector<string>> wordOccured; // another map to hold a vector of words that occured int times
+
+    std::ifstream openBook("meditations.txt"); // ifstream object to read from file
+
+    if(openBook.fail()){ // make sure nothing went wrong 
+        cout << "Error opening book. " << endl;
+    }
+    std::vector<string> allWords; // vector to hold every unique string in the book
+
+    string word; // initialize a string to hold input
+    while(openBook >> word) { // take in words from file
+        
+        if(numOccurences[word] >= 1) { // if the word has already appeared at least once
+            numOccurences[word]++; // increment the number of times word has occured 
+        } else {
+            numOccurences[word] = 1; // if word hasn't been seen before, create a new "bucket" in the map for that word
+            allWords.push_back(word); // and add the new word to the vector of all words
+        }
+    }
+
+
+
+    // UNNEEDED
+    // std::map<string,int>::iterator it; // create an iterator to go over the existing data structure
+    // int i = 0;
+
+    for (int i = 0; i < allWords.size(); i++) // iterate through the vector holding all the words
+    {                                                                   
+        wordOccured[numOccurences[allWords[i]]].push_back(allWords[i]); // the word at position i is used as the key for numOccurences, the number
+        // of occurences of that word is used as the key to the second data structure, mapping to a vector holding every word that
+        // occured numOccurences[allWords[i]] times. The word to be added is pushed back to the mapped vector
+    }
+
+    int n;
+    cout << "Enter a number to see which words occured that many times" << endl;
+    
+    std::cin >> n;
+    for (int i =0; i<wordOccured[n].size(); i++){
+    cout << wordOccured[n][i] << "  ||  ";
+    }
+
+    // UNNEEDED
+    // for(it = numOccurences.begin(); it != numOccurences.end(); ++it){
+    //     // numOccurences["the"];
+    //     wordOccured[numOccurences[allWords[i]]];
+
+    //     i++; // increment what word we're on
+    // }
+
+
+
+
+    openBook.close(); // close the stream object we worked with
+    return 0;
+}
